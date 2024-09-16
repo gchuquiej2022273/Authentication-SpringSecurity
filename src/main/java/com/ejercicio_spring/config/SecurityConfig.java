c8aac125be4eff727ac17cd7449afea39493fb6d
gchuquiej-2022273 <gchuquiej-2022273@kinal.edu.gt>
2024-09-16 12:02:28 -0600

    Se agrego el Swagger

--- src/main/java/com/ejercicio_spring/config/SecurityConfig.java
+++ src/main/java/com/ejercicio_spring/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authRequest ->
                         authRequest
                                 .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager->
